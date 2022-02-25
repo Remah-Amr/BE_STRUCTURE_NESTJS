2226df798cb6fdbf4e432080fc82d106c53ec98a
Remah Amr <remah.alarabi@hamza.solutions>
2022-02-25 21:13:36 +0200

    handle advanced search techniques

--- src/search/usersSearch.service.ts
+++ src/search/usersSearch.service.ts
@@ -1,179 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { User } from 'src/users/models/_user.model';
 @Injectable()
-export default class UsersSearchService {
-  index = 'students';
+export default class UsersSearchService implements OnModuleInit {
+  async onModuleInit(): Promise<void> {
+    try {
+      await this.elasticsearchService.indices.create({
+        index: 'stemming_test_3',
+        settings: {
+          analysis: {
+            filter: {
+              synonym_test: {
+                type: 'synonym',
+                synonyms: ['firm => company', 'love, enjoy'],
+              },
+              stemmer_test: {
+                type: 'stemmer',
+                language: 'english',
+              },
+            },
+            analyzer: {
+              my_analyzer_3: {
+                tokenizer: 'standard',
+                filter: ['lowercase', 'synonym_test', 'stemmer_test'],
+                type: 'custom',
+              },
+            },
+          },
+        },
+        mappings: {
+          properties: {
+            description: {
+              type: 'text',
+              analyzer: 'my_analyzer_3',
+            },
+          },
+        },
+      });
+    } catch (err) {}
+  }
+  index = 'stemming_test_3';
 
   constructor(private readonly elasticsearchService: ElasticsearchService) {}
 
   async indexUser(user: User) {
-    // return this.elasticsearchService.index({
-    //   index: this.index,
-    //   document: {
-    //     id: user.id,
-    //     username: user.username,
-    //   },
-    // });
-    await this.elasticsearchService.indices.create({
-      index: 'nodejs',
-      mappings: {
-        properties: {
-          join_field: {
-            type: 'join',
-            relations: {
-              department: 'employee',
-            },
-          },
-        },
-      },
-    });
-
-    await this.elasticsearchService.index({
-      index: 'nodejs',
-      id: '1',
+    return this.elasticsearchService.index({
+      index: this.index,
       document: {
-        name: 'Development',
-        join_field: 'department',
+        description: 'I love working for my firm!',
       },
     });
-    await this.elasticsearchService.index({
-      index: 'nodejs',
-      id: '2',
-      document: {
-        name: 'Marketing',
-        join_field: 'department',
-      },
-    });
-    await this.elasticsearchService.index({
-      index: 'nodejs',
-      routing: '1',
-      id: '3',
-      document: {
-        name: 'Bo Andersen',
-        age: 28,
-        gender: 'M',
-        join_field: {
-          name: 'employee',
-          parent: 1,
-        },
-      },
-    });
-    await this.elasticsearchService.index({
-      index: 'nodejs',
-      routing: '2',
-      id: '4',
-      document: {
-        name: 'John Doe',
-        age: 44,
-        gender: 'M',
-        join_field: {
-          name: 'employee',
-          parent: 2,
-        },
-      },
-    });
-    await this.elasticsearchService.index({
-      index: 'nodejs',
-      routing: '1',
-      id: '5',
-      document: {
-        name: 'James Evans',
-        age: 32,
-        gender: 'M',
-        join_field: {
-          name: 'employee',
-          parent: 1,
-        },
-      },
-    });
-    await this.elasticsearchService.index({
-      index: 'nodejs',
-      routing: '1',
-      id: '6',
-      document: {
-        name: 'Daniel Harris',
-        age: 52,
-        gender: 'M',
-        join_field: {
-          name: 'employee',
-          parent: 1,
-        },
-      },
-    });
-    await this.elasticsearchService.index({
-      index: 'nodejs',
-      routing: '2',
-      id: '7',
-      document: {
-        name: 'Jane Park',
-        age: 23,
-        gender: 'F',
-        join_field: {
-          name: 'employee',
-          parent: 2,
-        },
-      },
-    });
-
-    await this.elasticsearchService.index({
-      index: 'order',
-      routing: '1',
-      id: '9',
-      document: {
-        name: 'Christina Parker',
-        age: 29,
-        gender: 'F',
-        join_field: {
-          name: 'employee',
-          parent: 1,
-        },
-      },
-    });
-    1;
   }
 
   async search(text: string) {
-    // const body = await this.elasticsearchService.search({
-    //   index: 'nodejs',
-    //   query: {
-    //     has_child: {
-    //       type: 'employee',
-    //       inner_hits: {},
-    //       query: {
-    //         bool: {
-    //           must: [
-    //             {
-    //               range: {
-    //                 age: {
-    //                   gte: 50,
-    //                 },
-    //               },
-    //             },
-    //           ],
-    //           should: [
-    //             {
-    //               term: {
-    //                 'gender.keyword': 'M',
-    //               },
-    //             },
-    //           ],
-    //         },
-    //       },
-    //     },
-    //   },
-    // });
-    const body = await this.elasticsearchService.search({
-      index: 'order',
-      sort: [{ purchased_at: 'desc' }] as any,
+    return this.elasticsearchService.search({
+      index: this.index,
       query: {
-        match_all: {},
+        match: {
+          description: 'enjoy work',
+        },
+      },
+      highlight: {
+        fields: {
+          description: {},
+        },
       },
     });
-
-    return body;
-
-    // const hits = body.hits.hits;
-    // return hits.map((item) => item._source);
   }
 }
